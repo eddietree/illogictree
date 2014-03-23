c322d192081181cdb497564738cf099a5ddecd65
eddietree <eddie@illogictree.com>
2014-03-23 14:53:24 -0700

    added new projects into JSON

--- views/index.php
+++ views/index.php
@@ -9,16 +9,38 @@ $file = "http://localhost/public/data.json";
 $string = file_get_contents($file);
 $results = json_decode($string);
 
-foreach( $results as $key=>$data)
-{
-	echo $key;
-}
+// TODO: sort
+$projects = $results;
+/*
+echo "<code>";
+//var_dump($projects);
+echo "</code>";
+*/
 ?>
-
 
       <div class="starter">
         <h1><?= $this->pageTitle ?></h1>
-        <p class="lead"><?= $this->helloWorld ?></p>
+        <p class="lead">Real-time visuals and video games by <a href="#">Eddie Lee</a></p>
+
+        <div class="row">
+
+        	<?php foreach( $projects as $id => $project ) {?>
+
+        	<div class="col-sm-4" >
+        		<div class="project-item">
+        			<!--<img class="img-responsive" src ="<?= $this->app->getRootPath().'images/projects/'.$id.'.png' ?>"/>-->
+        			<a href="#"><img class="img-responsive" src ="<?= $this->app->getRootPath().'images/projects/sigma.png' ?>"/></a>
+        			<div class="info">
+        				<h3><?= $project->title;?></h3>
+        				<div class="desc"><?= $project->desc;?></div>
+        				<div class="desc-footer"><span class="date">March 3, 2014</span></div>
+        			</div>
+        		
+        		</div>
+        	</div>
+
+        	<?php }?>
+        </div>
       </div>
 
     </div><!-- /.container -->
