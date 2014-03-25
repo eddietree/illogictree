823209c9e0d042effd9d3b7f30e26f16c5da6763
eddietree <eddie@illogictree.com>
2014-03-25 19:44:03 +0900

    installed masonry

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,14 @@
-    <script src="<?= $this->app->getRootPath() ?>extlib/js/jquery-1.11.0.min.js"></script>
+    <script src="<?= $this->app->getRootPath() ?>extlib/jquery-1.11.0.min.js"></script>
+    <script src="<?= $this->app->getRootPath() ?>extlib/masonry.pkgd.min.js"></script>
     <script src="<?= $this->app->getRootPath() ?>extlib/bootstrap/js/bootstrap.min.js"></script>
+
+    <script>
+    var container = document.querySelector('#mason-container');
+		var msnry = new Masonry( container, {
+		  // options
+		 
+		  itemSelector: '.item'
+		});
+    </script>
   </body>
 </html>
--- views/index.php
+++ views/index.php
@@ -11,36 +11,38 @@ $results = json_decode($string);
 
 // TODO: sort
 $projects = $results;
+
 /*
 echo "<code>";
-//var_dump($projects);
-echo "</code>";
-*/
+var_dump($projects);
+echo "</code>";*/
+
 ?>
 
       <div class="starter">
         <h1><?= $this->pageTitle ?></h1>
         <p class="lead">Real-time visuals and video games by <a href="#">Eddie Lee</a></p>
 
-        <div class="row">
+        <div id="mason-container">
 
-        	<?php foreach( $projects as $id => $project ) {?>
+            <?php foreach( $projects as $id => $project ) {?>
 
-        	<div class="col-sm-4" >
-        		<div class="project-item">
-        			<!--<img class="img-responsive" src ="<?= $this->app->getRootPath().'images/projects/'.$id.'.png' ?>"/>-->
-        			<a href="#"><img class="img-responsive" src ="<?= $this->app->getRootPath().'images/projects/sigma.png' ?>"/></a>
-        			<div class="info">
-        				<h3><?= $project->title;?></h3>
-        				<div class="desc"><?= $project->desc;?></div>
-        				<div class="desc-footer"><span class="date">March 3, 2014</span></div>
-        			</div>
-        		
-        		</div>
-        	</div>
+            <div class="item <?php if( property_exists($project,'highlight') ) echo "w2" ?> ">
+                <div class="project-item">
+                    <!--<img class="img-responsive" src ="<?= $this->app->getRootPath().'images/projects/'.$id.'.png' ?>"/>-->
+                    <a href="#"><img class="img-responsive" src ="<?= $this->app->getRootPath().'images/projects/sigma.png' ?>"/></a>
+                    <div class="info">
+                        <h3><?= $project->title;?></h3>
+                        <div class="desc"><?= $project->desc;?></div>
+                        <div class="desc-footer"><span class="date">March 3, 2014</span></div>
+                    </div>
+                
+                </div>
+            </div>
 
-        	<?php }?>
+            <?php }?>
         </div>
+
       </div>
 
     </div><!-- /.container -->
